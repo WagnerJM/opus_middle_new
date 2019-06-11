c5efb93b92122fcc46dfc7876ac07b276961b9ca
Jean-Marc Wagner <jeanmarc.wagner@gmx.de>
2019-06-11 20:19:22 +0200

    added css to center form

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,21 +1,41 @@
 <template>
-  <div class="container">
+  <div class="container center">
     <h2>Login</h2>
-    <form>
+    <form @submit.prevent>
       <label for="username">First Name</label>
-      <input type="text" id="username" name="Username" placeholder="Username..">
+      <input v-model="username" type="text" id="username" name="Username" placeholder="Username..">
       
       <label for="password">Last Name</label>
-      <input type="password" id="password" name="Password" placeholder="Password..">
+      <input v-model="password" type="password" id="password" name="Password" placeholder="Password..">
       
-      <input type="submit" value="Submit">
+      <button @click="login">Login</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    login() {
+      const formData = {
+        username: this.username,
+        password: this.password
+      }
+      this.$store.dispatch("LOGIN", formData);
+    }
+  },
+  data() {
+    return {
+      username: "",
+      password: ""
+    }
+  },
+};
 </script>
 
 <style>
+.center{
+  margin: auto;
+  width: 50%;
+}
 </style>
